baa5ec778d445512b80b7aae943ede4a56a9137c
SIDDK24 <skoyande17@gmail.com>
2022-10-22 17:33:25 +0000

    Add extras for qssi-user 12 12 SKQ1.211019.001 V13.0.1.0.SJPINXM release-keys
    Signed-off-by: SIDDK24 <skoyande17@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 12
+12 SKQ1.211019.001 V13.0.1.0.SJPINXM release-keys
+- Manufacturer: xiaomi
+- Platform: atoll
+- Codename: gram
+- Brand: POCO
+- Flavor: qssi-user
+- Release Version: 12
+12
+- Id: SKQ1.211019.001
+- Incremental: V13.0.1.0.SJPINXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: POCO/gram_in/gram:12/RKQ1.211019.001/V13.0.1.0.SJPINXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-12
+12-SKQ1.211019.001-V13.0.1.0.SJPINXM-release-keys
+- Repo: poco_gram_dump
---
+++ join_split_files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk
+rm -f system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk
+rm -f system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null
+cat system/system/app/MIUINotes/MIUINotes.apk.* 2>/dev/null >> system/system/app/MIUINotes/MIUINotes.apk
+rm -f system/system/app/MIUINotes/MIUINotes.apk.* 2>/dev/null
+cat system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null >> system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk
+rm -f system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat bootimg/13_dtbdump_v'R\kt.dtb.* 2>/dev/null >> bootimg/13_dtbdump_v'R\kt.dtb
+rm -f bootimg/13_dtbdump_v'R\kt.dtb.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
